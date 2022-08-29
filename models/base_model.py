4d136006f35236def02b8daa988e53b0863d7349
Azeez Ibrahim <azeez2910@outlook.com>
2022-08-29 14:49:27 +0100

    update files

--- models/base_model.py
+++ models/base_model.py
@@ -3,4 +3,13 @@
    BaseModel defines all common attributes/methods
    for other classes
  """
+from datetime import datetime
+import uuid
+class BaseModel:
+    """BAseModel class"""
+    id = str(uuid.uuid4())
+    created_at = datetime.now()
+    updated_at = datetime.now()
 
+    def __str__(self):
+        
