aef6322dadc7ac087a47eb25e6c107063d413330
Azeez Ibrahim <azeez2910@outlook.com>
2022-08-31 23:43:14 +0100

    add __str__ method

--- models/base_model.py
+++ models/base_model.py
@@ -6,13 +6,13 @@
 from datetime import datetime
 import uuid
 class BaseModel:
-    """BAseModel class"""
+    """BaseModel class"""
     def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
     def __str__(self):
-        return pass
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
     def save(self):
         self.updated_at = datetime.now()
     def to_dict(self):
