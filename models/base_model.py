ca5bf034f60fb15f914ef60fe995e7aa34021f8f
Azeez Ibrahim <azeez2910@outlook.com>
2022-08-31 23:46:42 +0100

    fix pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -5,15 +5,22 @@
  """
 from datetime import datetime
 import uuid
+
+
 class BaseModel:
     """BaseModel class"""
+
     def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
     def __str__(self):
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
+
     def save(self):
         self.updated_at = datetime.now()
+
     def to_dict(self):
         return self.__dict__
