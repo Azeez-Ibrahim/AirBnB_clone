2339bcdb0301eef7c5b287270dbfd5ce7bd9b70d
Azeez Ibrahim <azeez2910@outlook.com>
2022-09-01 00:01:18 +0100

    add test file and to_dict method

--- models/base_model.py
+++ models/base_model.py
@@ -23,4 +23,8 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        return self.__dict__
+        class_dict = self.__dict__.copy()
+        class_dict["__class__"] = type(self).__name__
+        class_dict["created_at"] = class_dict["created_at"].isoformat()
+        class_dict["updated_at"] = class_dict["updated_at"].isoformat()
+        return class_dict
