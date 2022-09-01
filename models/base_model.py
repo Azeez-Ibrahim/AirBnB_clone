2575f7f6e6b5b789921029911e11a8c461a9f82d
Azeez Ibrahim <azeez2910@outlook.com>
2022-09-01 16:36:15 +0100

    re-create an instance with this dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -11,9 +11,16 @@ class BaseModel:
     """BaseModel class"""
 
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs is not None and kwargs != {}:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.fromisoformat(value))
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".\
