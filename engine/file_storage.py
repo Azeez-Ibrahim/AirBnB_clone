df18ef85fdb0a357bf19dc3f6711b3e54b69dc73
Azeez Ibrahim <azeez2910@outlook.com>
2022-09-02 23:02:39 +0100

    add reload method

--- engine/file_storage.py
+++ engine/file_storage.py
@@ -11,9 +11,12 @@ class FileStorage:
         key = "{}.{}".format(type(obj).__name__, id(obj))
         FileStorage.__objects[key] = obj
     def save(self):
-        with open( FileStorage.__file_path, "w", encoding="utf-8") as f:
-            dict_obj = {k: v.to_dict() for k, v in FileStorage.__objects.items()} 
-            json.dump(dict_obj, f)
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            obj_dict = {k: v.to_dict() for k, v in FileStorage.__objects.items()} 
+            json.dump(obj_dict, f)
     def reload(self):
-        
+        if os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+                dict_obj = json.load(f)
+
 
