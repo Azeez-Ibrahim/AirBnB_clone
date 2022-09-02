7d13df5854e981a97a068e77c8dc03d389e82eab
Azeez Ibrahim <azeez2910@outlook.com>
2022-09-02 23:26:05 +0100

    add storage to basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@
  """
 from datetime import datetime
 import uuid
-
+import storage
 
 class BaseModel:
     """BaseModel class"""
@@ -21,6 +21,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         return "[{}] ({}) {}".\
@@ -28,7 +29,7 @@ class BaseModel:
 
     def save(self):
         self.updated_at = datetime.now()
-
+        storage.save()
     def to_dict(self):
         class_dict = self.__dict__.copy()
         class_dict["__class__"] = type(self).__name__
