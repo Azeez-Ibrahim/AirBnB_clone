3f9ffdab77e67125ed554897ea820d16c2e89da8
Azeez Ibrahim <azeez2910@outlook.com>
2022-09-02 23:55:42 +0100

    add insert feature to reload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -23,3 +23,7 @@ class FileStorage:
         if os.path.exists(FileStorage.__file_path):
             with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
                 dict_obj = json.load(f)
+            from models.base_model import BaseModel
+            for k, v in dict_obj.items():
+                if v["__class__"] == "BaseModel"
+                    FileStorage.__objects[k] = BaseModel(**value)
