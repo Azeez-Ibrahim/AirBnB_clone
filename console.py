f2756feda3d064ffd1a880f4dec151db76f2a21b
Azeez Ibrahim <azeez2910@outlook.com>
2022-09-03 13:05:39 +0100

    fix pycode style

--- console.py
+++ console.py
@@ -3,22 +3,28 @@
 """AirBnB Console (Single use, or customize shell)"""
 import cmd
 
-# from models.base_model import BaseModel
-# from models.user import User
-# from models.state import State
-# from models.amenity import Amenity
-# from models.city import City
-# from models.review import Review
-# from models.place import Place
-# from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.amenity import Amenity
+from models.city import City
+from models.review import Review
+from models.place import Place
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
     """This is overall class for HBNBCommand"""
 
     prompt = "(hbnb) "
-    # classes = {'BaseModel': BaseModel, 'User': User, 'Place': Place,
-    #'State': State, 'City': City, 'Amenity': Amenity, 'Review': Review}
+    classes = {'BaseModel': BaseModel,
+                'User': User,
+                'Place': Place,
+                'State': State,
+                'City': City,
+                'Amenity': Amenity,
+                'Review': Review
+               }
 
     def do_quit(self, args):
         """This is quit method for exiting the program"""
