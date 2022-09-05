922cb6ae7bca9e38f6ae4426984c4096585852fc
Azeez Ibrahim <azeez2910@outlook.com>
2022-09-05 21:39:37 +0100

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -24,7 +24,8 @@ class FileStorage:
     def save(self):
         """Serializes __objects to JSON file"""
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
-            obj_dict = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+            obj_dict = {k: v.to_dict() for k, v in
+                        FileStorage.__objects.items()}
             json.dump(obj_dict, f)
 
     def reload(self):
@@ -33,7 +34,25 @@ class FileStorage:
             with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
                 dict_obj = json.load(f)
             from models.base_model import BaseModel
+            from models.user import User
+            from models.place import Place
+            from models.city import City
+            from models.state import State
+            from models.amenity import Amenity
+            from models.review import Review
 
             for k, v in dict_obj.items():
                 if v["__class__"] == "BaseModel":
-                    FileStorage.__objects[k] = BaseModel(**v)
+                    self.__objects[k] = BaseModel(**v)
+                elif v["__class__"] == "User":
+                    self.__objects[k] = User(**v)
+                elif v["__class__"] == "Place":
+                    self.__objects[k] = Place(**v)
+                elif v["__class__"] == "City":
+                    self.__objects[k] = City(**v)
+                elif v["__class__"] == "State":
+                    self.__objects[k] = State(**v)
+                elif v["__class__"] == "Amenity":
+                    self.__objects[k] = Amenity(**v)
+                elif v["__class__"] == "Review":
+                    self.__objects[k] = Review(**v)
