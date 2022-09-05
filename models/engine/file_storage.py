c180026f8ac62982ff51bc0cb5dc139f4cc01f3b
Azeez Ibrahim <azeez2910@outlook.com>
2022-09-05 09:12:26 +0100

    fix save method in console

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -24,7 +24,7 @@ class FileStorage:
     def save(self):
         """Serializes __objects to JSON file"""
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
-            obj_dict = {k: v.to_dict() /
+            obj_dict = {k: v.to_dict()
                         for k, v in FileStorage.__objects.items()}
             json.dump(obj_dict, f)
 
